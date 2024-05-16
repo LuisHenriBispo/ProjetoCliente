155112daa2265646a75cf48332c63cc1c4b234a9
Luís <duimbispo@gmail.com>
2024-05-16 15:31:14 -0300

    Criando o script do banco de dados

---
+++ Banco de Dados.sql
@@ -0,0 +1,13 @@
+/*criando banco de dados*/
+create database dbProjeto;
+/*use dbProjeto - usando banco de datos*/
+use dbProjeto;
+/*criando as tabelas do banco*/
+create table tbCliente (
+codCli int primary key auto_increment,
+nome varchar(50),
+telefone varchar(20),
+email varchar(50)
+);
+
+select * from tbCliente;
