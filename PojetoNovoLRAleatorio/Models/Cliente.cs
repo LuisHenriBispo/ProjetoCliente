2108afd588dd45c1254ee651e119501e00048cee
Luis <duimbispo@gamil.com>
2024-05-23 15:41:03 -0300

    linkando banco de dados

---
+++ PojetoNovoLRAleatorio/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetoNovoLRAleatorio.Models
+{
+    public class Cliente
+    {
+        //Criando encaspsulamento do objeto com (get set)
+        public int Id { get; set; }
+        
+        public string? Nome { get; set; }
+
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+
+        public List<Cliente>? ListaCliente { get; set; }
+
+
+    }
+}
---
+++ PojetoNovoLRAleatorio/Repositorio/IClienteRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using ProjetoNovoLRAleatorio.Models;
+
+namespace ProjetoNovoLRAleatorio.Repositorio
+{
+    public interface IClienteRepositorio
+    {
+        //crud
+        IEnumerable<Cliente> TodosClientes();
+    }
+}
